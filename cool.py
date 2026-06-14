cbe01b4bbcfd9c8a26c79036f1315d0ec8288750
ilkorplay2011 <ilia.korkiti@icloud.com>
2026-06-14 21:54:17 +0400

    updated 1.1

--- cool.py
+++ cool.py
@@ -5,36 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
-
-def calculate(a, b, oper):
-    match oper:
-        case '+':
-            return a + b
-        case '-':
-            return a - b
-        case '*':
-            return a * b
-        case '/':
-            if b == 0:
-                return "Нельзя делить на 0"
-            return round(a/b,4)
-        case '**':
-            return a ** b
-        case _:
-            return "Неизвестная операция"
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,'welcome')
+    bot.send_message(message.chat.id,'добро пожаловать на бот калькулятор')
 
 @bot.message_handler(commands=['calc'])
 def calc(message):
     bot.send_message(message.chat.id,'пример: 4+7')
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: not message.text.startswith('/'))
 def calculator(message):
     try:
-        result = simple_eval(message.text)
+        text = message.text
+        text = text.replace("×", "*")
+        text = text.replace("÷", "/")
+        text = text.replace("^", "**")
+
+        result = simple_eval(text)
 
         bot.send_message(
             message.chat.id,
